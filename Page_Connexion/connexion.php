f26b40ca0761c1511e1db210f23a3baf3a19cec4
lilyetudev <lily.fournier@etu.uca.fr>
2024-12-13 17:32:27 +0100

    Page de connexion
    Page de connexion v1

--- Page_Connexion/connexion.php
+++ Page_Connexion/connexion.php
@@ -37,7 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link href="CSS/contact.css" rel="stylesheet">
+    <link href="connexion.css" rel="stylesheet">
     
 </head>
 <body>
@@ -68,20 +68,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </div>
     </header>
 
-    <h1>Login</h1>
-    <?php if (isset($error)): ?>
-        <p style="color: red;"><?= htmlspecialchars($error) ?></p>
-    <?php endif; ?>
-    <form method="post" action="">
-        <label for="email">email:</label>
-        <input type="text" id="email" name="email" required>
-        <br>
-        <label for="password">Mot de passe :</label>
-        <input type="password" id="password" name="password" required>
-        <br>
-        <button type="submit">Login :</button>
-    </form>
+    <section class="connexion">
+    <div class="titre">
+        <h1>Connectez-vous à votre compte AFAJ</h1>
+        <?php if (isset($error)): ?>
+            <p style="color: red;"><?= htmlspecialchars($error) ?></p>
+        <?php endif; ?>
+    </div>
+    <div class="champsConnexion">
+        <form method="post" action="">
+            <div class="mail">
+                <input type="text" id="email" name="email" placeholder="Entrez votre adresse email" required>
+            </div>
+            <br>
+            <div class="mdp">
+                <input type="password" id="password" name="password" placeholder="Entrez votre mot de passe" required>
+            </div>
+            <br>
+            <div class="seConnecter">
+                <button class="btn1" type="submit">Se connecter</button>
+            </div>
+        </form>
+    </div>
+</section>
+
 </body>
+
 <footer>
         <div class="custom-footer">
             <div class="container-fluid">
