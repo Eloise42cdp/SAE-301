b553484cfe76cbd0fe0071dd99d2a503f3caf50e
lilyetudev <lily.fournier@etu.uca.fr>
2024-12-14 12:53:18 +0100

    Page Connexion v1
    Page connexion v1 (il manque la partie inscription)

--- Page_Connexion/connexion.php
+++ Page_Connexion/connexion.php
@@ -38,7 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link href="connexion.css" rel="stylesheet">
+    <link href="connexion.css?v=1.0" rel="stylesheet">
     
 </head>
 <body>
@@ -70,29 +70,39 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </header>
 
     <section class="connexion">
-    <div class="titre">
-        <h1>Connectez-vous à votre compte AFAJ</h1>
-        <?php if (isset($error)): ?>
-            <p style="color: red;"><?= htmlspecialchars($error) ?></p>
-        <?php endif; ?>
+    <div class="col">
+        <div class="titre">
+            <h1>Connectez-vous à votre compte AFAJ</h1>
+            <?php if (isset($error)): ?>
+                <p style="color: red;"><?= htmlspecialchars($error) ?></p>
+            <?php endif; ?>
+        </div>
+        <div class="champsConnexion">
+            <form method="post" action="">
+                <div class="mail">
+                    <input type="text" id="email" name="email" placeholder="Entrez votre adresse email" required>
+                </div>
+                <br>
+                <div class="mdp">
+                    <input type="password" id="password" name="password" placeholder="Entrez votre mot de passe" required>
+                </div>
+                <br>
+                <div class="seConnecter">
+                    <button class="btn1" type="submit">Se connecter</button>
+                </div>
+            </form>
+        </div>
     </div>
-    <div class="champsConnexion">
-        <form method="post" action="">
-            <div class="mail">
-                <input type="text" id="email" name="email" placeholder="Entrez votre adresse email" required>
-            </div>
-            <br>
-            <div class="mdp">
-                <input type="password" id="password" name="password" placeholder="Entrez votre mot de passe" required>
-            </div>
-            <br>
-            <div class="seConnecter">
-                <button class="btn1" type="submit">Se connecter</button>
-            </div>
-        </form>
-    </div>
-</section>
 
+    <div class="col">
+        <div class="titre">
+            <h1>Créez votre compte AFAJ et devenez membre</h1>
+        </div>
+    </div>
+    </section>
+
+
+    
 </body>
 
 <footer>
