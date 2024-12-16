f44641aebb3e780856c60e022187b4eab15dd55f
Eloïse_cdp <eloise.escuder.cdp@gmail.com>
2024-12-16 10:31:00 +0100

    Add files via upload

--- class/user.php
+++ class/user.php
@@ -1,15 +1,43 @@
-
 <?php
 
 // User class for authentication
 class User {
     private $bdd;
-    private $TypeMembre; // Admin ou Membre
+    public $IdUser;        // ID du membre connecté
+    public $role;    // Admin ou Membre
 
     public function __construct($db) {
+        if (!isset($_SESSION)) session_start();
         $this->bdd = $db;
+        $this->IdUser = $_SESSION['user_id'] ?? 0;
+        
+
+        // on charge les information de l'utilisateur
+        $this->setRole();
+
+        //echo "<br>class user";
     }
 
+    private function setRole() {
+        $query = "SELECT MembreType.nom AS role
+                  FROM membre
+                  INNER JOIN MT ON membre.Id_Membre = MT.Id_Membre
+                  INNER JOIN MembreType ON MT.Id_MembreType = MembreType.Id_MembreType
+                  WHERE membre.Id_Membre = :id";
+
+        $stmt = $this->bdd->prepare($query);
+        $stmt->bindParam(':id', $this->IdUser);
+        $stmt->execute();
+
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        $this->role = $result['role'] ?? 'membre'; // Par défaut, rôle "membre"
+    }
+
+    public function isAdmin() {
+        return $this->role === 'admin';
+    }
+
+    // Verifi si le login et passe sont correcte
     public function login($username, $password) {
         $query = "SELECT * FROM membre WHERE email = :email";
         $stmt = $this->bdd->prepare($query);
@@ -19,7 +47,14 @@ class User {
         if ($stmt->rowCount() > 0) {
             $user = $stmt->fetch(PDO::FETCH_ASSOC);
             if (password_verify($password, $user['mdp'])) {
-                $_SESSION['user_id'] = $user['Id_membre'];
+                // Lier la session à des informations uniques
+                // $_SESSION du cote serveur
+                // Cookis cote navigateur
+                // Liaison entre navigateur et serveur avec PHPSESSID
+                session_regenerate_id(true); // Securite Session Cookis
+                $_SESSION['user_id'] = $user['Id_membre'];              // ID Membre 
+                $_SESSION['ip_address'] = $_SERVER['REMOTE_ADDR'];      // Adresse IP de l'utilisateur
+                $_SESSION['user_agent'] = $_SERVER['HTTP_USER_AGENT'];  // User-Agent du navigateur
                 $_SESSION['username'] = $user['nom']." ".$user['prenom'];
                 return true;
             }
@@ -36,9 +71,18 @@ class User {
         header("Location: connexion.php");
     }
 
-    // foncton pour savoir si le membre connecter est admin ou membre
-    //public GetTypeMembre() {
-        
-    //}
+    // foncton pour charger les information du membre
+    // membre connecter est admin ou membre
+    // 
+    public function GetInfoMembre() {
+        $query = "SELECT * FROM membre WHERE email = :email";
+        $stmt = $this->bdd->prepare($query);
+        $stmt->bindParam(":email", $username);
+        $stmt->execute();
+
+        $this->IdUser = $_SESSION['user_id'];
+        $this->TypeMembre = "admin";
+    }
 }
+
 
